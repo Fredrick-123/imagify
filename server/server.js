0a60dc9d1cdcded3f83480e443413baced92bfa8
Fredrick-123 <fredricknicholas8@gmail.com>
2025-12-07 19:05:23 +0100

    serverjs updated

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import connectDB from '../server/config/mongoDB.js';
 import userRouter from './routes/userRoutes.js';
 import imageRouter from './routes/imageRoutes.js';
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
 const app = express();
 
@@ -19,6 +19,10 @@ app.get('/test', (req,res)=>{
     res.send('API is working fine');
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+if (process.env.NODE_ENV !== "production"){
+ const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
+
+}
